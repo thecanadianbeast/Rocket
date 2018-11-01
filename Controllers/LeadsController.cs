893df46f03ad3d1592d12cc1dc97e739838482a7
Luna-Victoria <42751401+Luna-Victoria@users.noreply.github.com>
2018-11-01 11:36:53 -0400

    LeadsController

--- Controllers/LeadsController.cs
+++ Controllers/LeadsController.cs
@@ -1,48 +1,40 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
+using Microsoft.EntityFrameworkCore;
 using Rocket.Models;
 
-namespace Rocket.Controllers {
-
+namespace Rocket.Controllers
+{
     [Route ("api/leads")]
     [ApiController]
     public class LeadsController : ControllerBase {
-        
-        private readonly David_appContext _context;
-
-        public LeadsController (David_appContext context) {
+        private readonly MySql_appContext _context;
+        public LeadsController (MySql_appContext context) {
             _context = context;
         }
 
-
+        // GET api/leads
         [HttpGet]
         public ActionResult<List<Leads>> GetAll () {
-            var listl = _context.Leads.ToList ();
-            var listc = _context.Customers.ToList ();
-            // var duration = new System.TimeSpan(-30, 0, 0, 0);
-            // var month = DateTime.Now.Add(duration);
+            var listl = _context.Leads.Include (le => le.Customers);
+
             if (listl == null) {
-                return NotFound ("Not Found");
-            }
-            if (listc == null) {
                 return NotFound ("Not Found");
             }
 
             List<Leads> list_lead = new List<Leads> ();
-            DateTime currentDate = DateTime.Now.AddDays(-30);
-
+            DateTime currentDate = DateTime.Now.AddDays (-30);
             foreach (var l in listl) {
-
-                if (l.CreatedAt >= currentDate ) {
-                    list_lead.Add (l);
+                if (l.CreatedAt >= currentDate) {
+                    if (l.Customers.ToList().Count == 0) {
+                        list_lead.Add (l);
+                    }
                 }
             }
+    
             return list_lead;
         }
-
     }
 }
