7b59c203a8c8743f2584774ec2dc065252622c00
dboutin84 <d.boutin84@gmail.com>
2018-11-21 11:05:36 -0500

    Adresse_controller

---
+++ Controllers/AddressController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Rocket.Models;
+
+namespace Rocket.Controllers {
+
+    // [Route ("api/elevators")]
+    [Route ("api/city")]
+    [ApiController]
+    public class AddressesControllers : ControllerBase {
+        private readonly MySql_appContext _context;
+
+        public AddressesControllers (MySql_appContext context) {
+            _context = context;
+        }
+
+        // get all customers
+        [HttpGet]
+        public ActionResult<List<string>> GetAll () {
+
+            var _result = _context.Addresses.Select (s => s.City).Distinct ();
+
+            return _result.ToList ();
+
+
+        }
+    }
+}
